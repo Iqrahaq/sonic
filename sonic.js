41f3ad82b5701c3aaf7667a655fe3023bc12bf64
Iqrahaq <iqrahaq@live.co.uk>
2018-07-13 12:51:15 +0100

    sonic

---
+++ sonic.js
@@ -0,0 +1,25 @@
+//Snake
+
+//Duplication of div tags to create 15x15 grid.
+function addDiv(){
+    var parent = document.getElementById("gridContainer");
+    var newDiv = document.createElement("div");
+    parent.appendChild(newDiv);
+    newDiv.setAttribute("class", "gridBox");
+}
+
+for(var i=0;i<225;i++){
+    addDiv();
+}
+
+//Checkered grid
+var checkered = document.getElementsByClassName("gridBox");
+for(var n=0;n<225;n++){
+   checkered[n].style.backgroundColor = "black";
+   n++;
+}
+
+var grid = document.getElementsByClassName("grid-box");
+
+
+
